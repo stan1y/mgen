c0e0ac9ef1e35910b603a47164449778e677bf24
Stanislav Yudin <stan@endlessinsomnia.com>
2016-11-07 02:17:39 +0000

    add wizard as jquery plugin

---
+++ mgen/web/static/js/wizard.js
@@ -0,0 +1,116 @@
+/*
+ * MGEN Wizard jQuery plugin
+ */
+ 
+(function ( $ ) {
+    
+    var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend'
+    
+    var Wizard = function(el, options) {
+        this.options = $.extend({
+            animate: true,
+            animateShow: "fadeIn",
+            animateHide: "fadeOut",
+            btnNext: "wizard-next",
+            btnPrev: "wizard-prev",
+            btnNextText: "Next",
+            btnFinishText: "Finish",
+            btnBackText: "Back",
+            btnCancelText: "Cancel"
+        }, options)
+        
+        this.el = $(el)
+        this.steps = this.el.find('div.step')
+        this.currentStep = $(this.el.find('div.step.first')[0])
+        this.currentStepIdx = 0
+        this.btnNext = $('#' + this.options.btnNext)
+        this.btnPrev = $('#' + this.options.btnPrev)
+        
+        var self = this;
+        this.btnNext.click(function() {
+            self.forward()
+        })
+        this.btnPrev.click(function() {
+            self.back()
+        })
+        
+        this.steps.hide()
+        this.currentStep.show()
+        return this
+    }
+    
+    Wizard.prototype._updateBtns = function() {
+        
+        if (this.currentStepIdx >= this.steps.length - 1) {
+            // reached end
+            this.btnNext.text(this.options.btnFinishText)
+            this.btnPrev.text(this.options.btnBackText)
+        }
+        else {
+            this.btnNext.text(this.options.btnNextText)
+            if (this.currentStepIdx == 0) {
+                this.btnPrev.text(this.options.btnCancelText)
+            }
+        }
+    }
+    
+    Wizard.prototype.switch = function(idx) {
+        var nextStep = this.steps[idx],
+            toshow = $(nextStep),
+            tohide = $(this.currentStep)
+
+        if (this.options.animate) {
+            tohide.show()
+            toshow.show()
+            
+            toshow.css({position: 'absolute'})
+            tohide.css({position: 'relative'})
+            tohide.css({float: 'left'})
+            
+            toshow.one(animationEnd, function() {
+                tohide.hide()
+                toshow.css({position: ''})
+                tohide.css({position: ''})
+                tohide.css({float: ''})
+            })
+            
+            tohide.animateCss(this.options.animateHide)
+            toshow.animateCss(this.options.animateShow)
+        }
+        else {
+            tohide.hide()
+            toshow.show()
+        }
+        this.currentStep = $(nextStep)
+        this._updateBtns()
+    }
+    
+    Wizard.prototype.forward = function() {
+        if (this.currentStepIdx >= this.steps.length - 1) {
+            // at the last step
+            return
+        }
+        this.currentStepIdx += 1
+        this.switch(this.currentStepIdx)
+    }
+    
+    Wizard.prototype.back = function() {
+        if (this.currentStepIdx == 0) {
+            // at the first step
+            return
+        }
+        this.currentStepIdx -= 1
+        this.switch(this.currentStepIdx)
+    }
+ 
+    $.fn.wizard = function(options) {
+        return new Wizard(this, options)   
+    }
+    
+    $.fn.animateCss = function (animationName) {
+        this.addClass('animated ' + animationName).one(animationEnd, function() {
+            $(this).removeClass('animated ' + animationName)
+        })
+    }
+ 
+}( jQuery ));
