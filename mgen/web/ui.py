85c5c9cfdeadde0e6f549b9cffd6b983ae2d145c
Stanislav Yudin <stan@endlessinsomnia.com>
2016-11-19 03:28:57 +0000

    project permissions mixin

--- mgen/web/ui.py
+++ mgen/web/ui.py
@@ -18,6 +18,22 @@ import tornado.template
 
 log = logging.getLogger(__name__)
 
+class ProjectPermissionMixin(object):
+    '''Providers easy access to project permissions'''
+    
+    def get_permissions(self, proj):
+        p = proj.get_permission(self.current_profile.email)
+        if Permission.Forbidden & p:
+            raise mgen.error.Forbidden().describe('access denied to project id "%s"' % project_id)
+        # permissions
+        return {
+            'forbidden': (Permission.Forbidden & p),
+            'allow_edit' : (Permission.Edit & p),
+            'allow_create' : (Permission.Create & p),
+            'allow_build' : (Permission.Build & p),
+            'allow_deploy' : (Permission.Deploy & p),
+            'is_owner' : (Permission.GrantPermisson & p)
+        }
 
 class Overview(BaseRequestHandler):
     @authenticated
@@ -27,15 +43,14 @@ class Overview(BaseRequestHandler):
                            user=self.current_user,
                            profile=self.current_profile)
 
-
-class Project(BaseRequestHandler):
+class Project(BaseRequestHandler, ProjectPermissionMixin):
     @authenticated
     def get(self, oid):
         s = session()
         proj = s.query(mgen.model.Project).filter_by(project_id=oid).one()
-        p = proj.get_permission(self.current_profile.email)
-        if Permission.Forbidden & p:
-            raise mgen.error.Forbidden().describe('access denied to project id "%s"' % oid)
+        p = self.get_permissions(proj)
+        if p['forbidden']:
+             raise mgen.error.Forbidden().describe('access denied to project id "%s"' % oid)
         
         log.debug('displaying project %s' % proj.project_id)
         return self.render("project.html", 
@@ -44,15 +59,25 @@ class Project(BaseRequestHandler):
                            project=proj,
                            item_types=mgen.generator.item_types,
                            template_types=mgen.generator.template.template_types,
-                           # permissions
-                           allow_edit=(Permission.Edit & p),
-                           allow_create=(Permission.Create & p),
-                           allow_build=(Permission.Build & p),
-                           allow_deploy=(Permission.Deploy & p),
-                           is_owner=(Permission.GrantPermisson & p))
+                           **p
+                          )
                            
-class Template(BaseRequestHandler):
-    pass
+class Template(BaseRequestHandler, ProjectPermissionMixin):
+    @authenticated
+    def get(self, oid):
+        s = session()
+        tmpl = s.query(mgen.model.Template).filter_by(template_id=oid).one()
+        p = self.get_permissions(tmpl.project)
+        if p['forbidden']:
+             raise mgen.error.Forbidden().describe('access denied to project id "%s"' % tmpl.project_id)
+        
+        log.debug('displaying template %s' % tmpl.template_id)
+        return self.render("project.html", 
+                           user=self.current_user,
+                           profile=self.current_profile,
+                           the_template=tmpl,
+                           template_types=mgen.generator.template.template_types,
+                           **p)
 
 class Item(BaseRequestHandler):
     pass
